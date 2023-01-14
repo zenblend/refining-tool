1dd7de62ced75a6c60cca59f1e8b4c92a38bfc73
Nick Orr <new.west1983@gmail.com>
2023-01-14 09:51:44 -0800

    a handful of css updates and code touchup -TODO: rewrite item rendering logic and recipe data structure to feed recipe dynamically

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Content from './Components/Content/Content'
 import Footer from './Components/Footer/Footer'
+import { Top } from './Components/Top/Top'
 
 function App() {
 
   return (
     <div id="app">
+      <Top />
       <Content />
       <Footer />
     </div>
---
+++ src/Components/Top/Top.tsx
@@ -0,0 +1,10 @@
+import './Top.css'
+
+export function Top() {
+    return (
+        <section id="top">
+            <h1 id="top-title">Refining Tool</h1>
+        </section>
+
+    )
+}
